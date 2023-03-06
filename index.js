a4e82c28a4a0798a5556c7b67d4dcab62cfafde0
jonathon lee <jonnyleealas@gmail.com>
2023-03-06 11:26:50 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 const reactContentRoot = document.getElementById("root")
+const inputTest = React.createElement("input")
+
 const myFirstElement = React.createElement("ul",null, 
     React.createElement = React.createElement(
         "li", null, [
             React.createElement("li", null, "item1"),
             React.createElement("li", null, "item2"),
-            React.createElement("li", null, "item3")
+            React.createElement("li", null, "item3"),
+            React.createElement("li", null, "whats up doc"),
+            inputTest
         ]
     )
 )
+
+
 ReactDOM.render(myFirstElement, reactContentRoot)
