44f24a1f734b8efc5a224ed9cda96991b7cebad3
jonathon lee <jonnyleealas@gmail.com>
2023-03-07 08:52:44 -0600

    jsx

--- index.js
+++ index.js
@@ -3,17 +3,14 @@
 const reactContentRoot = document.getElementById("root")
 const inputTest = React.createElement("input")
 
-const myFirstElement = React.createElement("ul",null, 
-    React.createElement = React.createElement(
-        "li", null, [
-            React.createElement("li", null, "item1"),
-            React.createElement("li", null, "item2"),
-            React.createElement("li", null, "item3"),
-            React.createElement("li", null, "whats up doc"),
-            inputTest, inputTest
-        ]
-    )
+const myJSXElement = (
+    <ul>
+        <li>item1</li>
+        <li>item2</li>
+        <li>item3</li>
+        <li>whats up doc</li>
+    </ul>
 )
 
 
-ReactDOM.render(myFirstElement, reactContentRoot)
+ReactDOM.render(myJSXElement, reactContentRoot)
