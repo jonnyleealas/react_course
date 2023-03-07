321ae7f799effd3f8c139b5394156bb8bc96515d
jonathon lee <jonnyleealas@gmail.com>
2023-03-07 08:58:41 -0600

    refresher on jsx
    Jsx will take html like code and convert it to java script code for react to read. Then ReactDOM will render it to html through our script tag. We also need to add babel our transpiler to html head.  With babel we can interpolate js values into our babel code, meaning we can write java script using bracket {}

--- index.js
+++ index.js
@@ -3,12 +3,15 @@
 const reactContentRoot = document.getElementById("root")
 const inputTest = React.createElement("input")
 
+
+const item = "whats up dude"
 const myJSXElement = (
     <ul>
         <li>item1</li>
         <li>item2</li>
         <li>item3</li>
         <li>whats up doc</li>
+        <li>{item.toLocaleUpperCase()}</li>
     </ul>
 )
 
