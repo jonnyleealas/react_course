ba8f16f98fafa6df0800318bf43c322e7b5c284d
jonathon lee <jonnyleealas@gmail.com>
2023-03-13 21:45:05 -0500

    children props and fetch

--- refresher/src/components/App.js
+++ refresher/src/components/App.js
@@ -1,46 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import CounterButton from './countButton/CounterButton';
 import SearchBar from './SearchBar/SearchBar';
+import Button from './Button/Button.js';
 import spinner from './images/spinner.avif';
 
 
-const sportsTeams = [
-    'lakers',
-    'celtics',
-    'clippers',
-    'real madrid', 
-    'yankees'
-]
-
-const ufcFighters = [
-    'jon jones',
-    'daniel cormier',
-    'conor mcgregor',
-    'anderson silva',
-    'georges st pierre',
-]
 
 
 const App = () => {
     const [productsState, setProductState] = useState([])
 
-
     useEffect(() => {
-        setTimeout(() => {
-            setProductState([
-                'tooth brush',
-                'tooth paste',
-                'mouth wash',
-                'dental floss',
-                'mouth guard',
-            ])
-        }, 5000)
+        fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then((productsArray) => {
+                const newProductsState = productsArray.map((product) => {
+                    return product.description
+                })
+                setProductState(newProductsState)
+            })       
     }, [])
 
     const hasProducts = productsState.length > 0
      
     return (
         <>
+        <Button>Hello World</Button>
         {
             hasProducts ? <SearchBar products={productsState} /> : <img src={spinner} alt="Italian Trulli" />
         }
